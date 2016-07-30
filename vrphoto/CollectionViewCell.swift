770cd3e598de70e469f0fce0e3af4e559b11969c
DavidKakaladze <Davjob15@gmail.com>
2016-07-30 13:29:40 +0300

    Add CollectoonViewCell

---
+++ vrphoto/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  vrphoto
+//
+//  Created by David Kakaladze on 30.07.16.
+//  Copyright © 2016 iosDevelopClub. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var label: UILabel!
+}
--- vrphoto/ViewController.swift
+++ vrphoto/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! CollectionViewCell
+        
+        cell.label.text = String(indexPath.row)
+        cell.backgroundColor = UIColor.lightGrayColor()
+        
+        return cell
+     }
+    
+    func collectionView(collectionView:UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+    
+        let size = CGSize(width: 80, height: 80)
+        return size
+    
+    }
+    
+    
 
 }
 
